14ff138e6011fe957be20c099a959f2a94d5a523
Carlos Encine <carlos.encine@outlook.com>
2023-10-29 15:10:52 -0300

    fix: fix fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "@/app/ui/home.module.css"
 import Image from "next/image"
 
 import Link from "next/link"
-import { lusitana } from "./ui/fonts"
+import { lusitana } from "@/app/ui/fonts"
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
